34fa46489731aa00de5efad5071ef0873878d654
leonardo silva <leonardovgds21@icloud.com>
2026-05-23 13:19:58 -0300

    setup initial settings for application and BD

---
+++ src/main/java/com/leonardo_silva/LifeQuest/LifeQuestApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo_silva.LifeQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardo_silva/LifeQuest/LifeQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardo_silva.LifeQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
